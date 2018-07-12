55c284d532a4c4f87ec4d004bdf06e240d18ad50
Ion <ionglavan@mail.ru>
2018-07-12 15:26:37 +0300

    am adaugat principalele clase

--- FluentApiUniver/Program.cs
+++ FluentApiUniver/Program.cs
@@ -6,6 +6,41 @@ using System.Threading.Tasks;
 
 namespace FluentApiUniver
 {
+
+    public class Profesor
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public List<Grupa> GetGrupas { get; set; }
+        public List<Curs> GetCurs { get; set; }
+    }
+
+    public class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public List<Curs> GetCurs { get; set; }
+        public Grupa GetGrupa { get; set; }
+    }
+
+    public class Curs
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public List<Grupa> GetGrupas { get; set; }
+        public Profesor GetProfesor { get; set; }
+    }
+
+    public class Grupa
+    {
+        public int ID { get; set; }
+        public string Nume { get; set; }
+        public List<Student> GetStudents { get; set; }
+        public List<Curs> GetCurses { get; set; }
+    }
+
+    
+
     class Program
     {
         static void Main(string[] args)
