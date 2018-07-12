dd4f2fa3667b25cfb71ac851b2460dfe5bd74489
Ion <ionglavan@mail.ru>
2018-07-12 15:16:18 +0300

    Add project files.

---
+++ FluentApiUniver/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentApiUniver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
